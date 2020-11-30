7ada4808dee2b264b03cfb14cbcc89ee4c828863
unknown <jasoncwy@hotmail.com>
2020-11-29 23:00:44 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// console.log('this is a test');
+
+$(document).ready(function(){
+    $('.cards-group').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        autoplay: true,
+    });
+    var filtered = false;
+
+    $('.js-filter').on('click', function(){
+    if (filtered === false) {
+        $('.cards-group').slick('slickFilter',':even');
+        $(this).text('Unfilter Slides');
+        filtered = true;
+    } else {
+        $('.cards-group').slick('slickUnfilter');
+        $(this).text('Filter Slides');
+        filtered = false;
+    }
+    });
+      
+});
